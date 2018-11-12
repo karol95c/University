49156fe24d8d2238ea040d98cac7c5f1171e83e0
karol95c <karol95c@gmail.com>
2018-11-12 13:04:16 +0100

    Added Zadanie4 (ShuntingYard)

---
+++ C++/Zadanie4/ShuntingYard.cpp
@@ -0,0 +1,121 @@
+#include "ShuntingYard.hpp"
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cctype>
+
+
+ShuntingYard::ShuntingYard(std::vector<std::string>& toConv) : toConvert(toConv) {}
+
+bool ShuntingYard::isOperator(char c) const
+{
+    return operatorPriority.find(c) != operatorPriority.end();
+}
+
+bool ShuntingYard::firstCase(char c) const
+{
+    return operators.top() != '^' && operatorPriority[operators.top()] >= operatorPriority[c];
+}
+
+bool ShuntingYard::secondCase(char c) const
+{
+    return operators.top() == '^' && operatorPriority[operators.top()] > operatorPriority[c];
+}
+
+void ShuntingYard::addToQueue(std::string str)
+{
+    input = std::queue<char>();
+    for (char c : str)
+    {
+        input.push(c);
+    }
+}
+
+void ShuntingYard::toONP(std::string expression)
+{
+    std::string result = "";
+    addToQueue(expression);
+    while (!input.empty())
+    {
+        char p = input.front();
+        input.pop();
+        if (p == ' ') continue;
+        else if (isdigit(p) or isalpha(p))
+        {
+            output.push(p);
+        }
+        else if (isOperator(p))
+        {
+            if (!operators.empty())
+            {
+                while (isOperator(operators.top()) and (firstCase(p) or secondCase(p)))
+                {
+                    output.push(operators.top());
+                    operators.pop();
+                }
+            }
+            operators.push(p);
+
+        }
+        else if (p == '(')
+        {
+            operators.push(p);
+        }
+        else if (p == ')')
+        {
+            while (operators.top() != '(')
+            {
+                output.push(operators.top());
+                operators.pop();
+            }
+            operators.pop();
+        }
+
+
+    }
+    while (!operators.empty())
+    {
+        output.push(operators.top());
+        operators.pop();
+    }
+
+    while  (!output.empty())
+    {
+        result += output.front();
+        output.pop();
+    }
+    std::cout << result + "\n";
+}
+
+void ShuntingYard::convertToONP()
+{
+    for (std::string str : toConvert)
+    {
+        toONP(str);
+    }
+}
+
+std::map<char, int> ShuntingYard::operatorPriority = {
+    {'+', 1},
+    {'-', 1},
+    {'*', 2},
+    {'/', 2},
+    {'%', 2},
+    {'^', 3}
+};
+
+int main()
+{
+    std::vector<std::string> testVector;
+    testVector.push_back("(2+a)+10/20");
+    testVector.push_back("3+4*2/(1-5)^2");
+    testVector.push_back("12 + a * (b * c + d / e)");
+    ShuntingYard *testPtr = new ShuntingYard(testVector);
+    testPtr->convertToONP();
+    delete testPtr;
+
+    return 0;
+}
---
+++ C++/Zadanie4/ShuntingYard.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+
+class ShuntingYard
+{
+    private:
+    std::vector<std::string> toConvert;
+    std::queue<char> input;
+    std::stack<char> operators;
+    std::queue<char> output;
+    static std::map<char, int> operatorPriority;
+    void addToQueue(std::string);
+
+    bool isOperator(char) const;
+    bool higherOrEqualPriority(char, char) const;
+    bool firstCase(char) const;
+    bool secondCase(char) const;
+    void toONP(std::string);
+
+    public:
+    ShuntingYard(std::vector<std::string>& toConv);
+    void convertToONP();
+
+};
